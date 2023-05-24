ba855f6f58a861d2a374343b1566efb2d2f82657
Teguh Widodo <teguhwin8@gmail.com>
2023-05-24 22:38:14 +0700

    edit leaflet

--- app/page.js
+++ app/page.js
@@ -10,47 +10,47 @@ export default function Home() {
     const eleaflets = [
         {
             title: "E-Leaflet 1",
-            description: "Tata Cara Penggunaan Antibiotik 1",
-            image: "e-leaflet-1.jpg",
+            description: "Petunjuk Penggunaan Obat Antibiotik",
+            image: "eleaflet_1.jpg",
         },
         {
             title: "E-Leaflet 2",
-            description: "Tata Cara Penggunaan Antibiotik 2",
-            image: "e-leaflet-2.jpg",
+            description: "Apa itu Antibiotik?",
+            image: "eleaflet_2.jpg",
         },
         {
             title: "E-Leaflet 3",
-            description: "Tata Cara Penggunaan Antibiotik 3",
-            image: "e-leaflet-3.jpg",
+            description: "Apa itu Resistensi?",
+            image: "eleaflet_3.jpg",
         },
         {
             title: "E-Leaflet 4",
-            description: "Tata Cara Penggunaan Antibiotik 4",
-            image: "e-leaflet-4.jpg",
+            description: "Ayo gunakan antibiotik secara bijak dan tepat",
+            image: "eleaflet_4.jpg",
         },
         {
             title: "E-Leaflet 5",
-            description: "Tata Cara Penggunaan Antibiotik 5",
-            image: "e-leaflet-5.jpg",
+            description: "Waktu pemakaian obat",
+            image: "eleaflet_5.jpg",
         },
         {
             title: "E-Leaflet 6",
-            description: "Tata Cara Penggunaan Antibiotik 6",
-            image: "e-leaflet-6.jpg",
+            description: "Tanyakan Apoteker",
+            image: "eleaflet_6.jpg",
             link: "https://docs.google.com/forms/d/e/1FAIpQLSeSCpcfIuZaRH2pYUEh6F0WhCwemGJ6wa-CGMFCrg8Jya507Q/viewform?usp=sf_link",
         },
     ];
     return (
         <>
-            <div className="h-screen w-screen max-w-3xl mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="min-h-screen w-screen max-w-5xl mx-auto my-5">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                     {eleaflets.map((eleaflet, index) => (
                         <Link href={eleaflet.link || "#"} key={index} target={eleaflet.link && "_blank"}>
                             <Image
-                                src={`/${eleaflet.image}`}
+                                src={`/eleaflet_image/${eleaflet.image}`}
                                 className="w-full"
-                                width={500}
-                                height={500}
+                                width={256}
+                                height={540}
                                 alt={eleaflet.description}
                             />
                         </Link>
